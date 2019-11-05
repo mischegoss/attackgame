9985db005027b18a294ebf458e0459f64de627db
Tamar Auber <tamarauber@Tamars-MBP.cable.rcn.com>
2019-11-04 19:59:35 -0500

    Got images from Giphy

---
+++ gifs.js
@@ -0,0 +1,121 @@
+$(document).ready(function() {
+  // Sets up initial array for button population
+
+  const pulled = $("#pulled-container");
+  const buttons = $("#button-row");
+  let limit = 10;
+
+  //Setting Initial Array Values
+  let dancingthings = ["puppies", "owls", "babies", "old people"];
+
+  /* This first empties out the button row using empty() and then uses a for loop to create a button */
+  /* Moved newButton var inside loop to create multiple buttons */
+
+  function makeButtons() {
+    buttons.empty();
+
+    for (var i = 0; i < dancingthings.length; i++) {
+      const newButton = $("<button>");
+
+      newButton.addClass("btn-click");
+      newButton.addClass("top-button");
+
+      newButton.attr("value", dancingthings[i]);
+
+      newButton.text(dancingthings[i]);
+
+      buttons.append(newButton);
+    }
+  }
+
+  /* This makes the buttons clickable and calls displayGifs */
+
+  //$(document).on("click", ".btn-click", displayGifs);
+
+  /* This  uses Ajax to pull the Gifs */
+
+  function displayGifs() {
+    pulled.removeClass("hide");
+
+    // This puts together the query URL used in AJAX
+    let input = "babies";
+    let api = "&api_key=dy0FmMLlA7vphv8GPrWfz8W18KNS9npL&limit=" + 4;
+    let queryURL =
+      "https://api.giphy.com/v1/gifs/search?q=" + "dancing " + input + api;
+
+    //The AJAX call
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+      item = response.data;
+
+     
+      for (var i = 0; i < 4; i++) {
+        // This  prevent R Rated Gifs
+       
+         
+          // Creates Image
+          stillimage = item[i].images.fixed_height_still.url;
+          console.log(stillimage)
+          movingimage = item[i].images.fixed_height.url;
+          
+
+          image = $("<img>");
+          image.attr("src", stillimage);
+          image.attr("data-still", stillimage);
+          image.attr("data-state", "still");
+          image.attr("data-animate", movingimage);
+          image.addClass("animate");
+          image.addClass("img-fluid");
+
+         
+          // Appends image and rating to new Item and newItem to hardcoded div
+          
+          $("stan-img").append(image);
+          
+        }
+      
+    });
+  //}
+
+displayGifs();
+  /* I tried to use '$(".animate") but it didn't work. I found the 
+  * document.on('click...) via Google */
+
+  $(document).on("click", ".animate", function() {
+    datastate = $(this).attr("data-state");
+
+    switch (datastate) {
+      case "still":
+        $(this).attr("src", $(this).data("animate"));
+        $(this).attr("data-state", "animate");
+        break;
+      case "animate":
+        $(this).attr("src", $(this).data("still"));
+        $(this).attr("data-state", "still");
+        break;
+    }
+  });
+
+  /* This is submit click event. No click if less than 3 letters entered to
+  * prevent  'bad' submits. */
+  $("#submit").on("click", function(event) {
+    event.preventDefault();
+    var newInput = $("#form-input")
+      .val()
+      .trim();
+    var checkInput = dancingthings.includes(newInput);
+
+    if (checkInput) {
+      $("#form-input").val("");
+    } else if (newInput.length > 2 && !checkInput) {
+      dancingthings.push(newInput);
+      $("#form-input").val("");
+      makeButtons();
+   // }
+  }//);
+
+  /* Calls MakeButton function to make initial buttons on load */
+  makeButtons();
+}); // Document ready end bracket
--- script.js
+++ script.js
@@ -2,41 +2,116 @@ const pickerLine = $(".pickerLine");
 const template = $("#template");
 const cardTitle = $(".card-title");
 const card = $('.card');
+let dancerOne;
+let dancerTwo;
+let isDancerOnePicked = false;
+let pickedName = "carla";
+
+let pickedNameArray = ["carla", "ralph"]
 
 
 
 class Dancer {
 
-    constructor(name, score, image, dance)  {
+    constructor(name, score, image)  {
+        
         this.name = name;
-        this.score = score;
+        this.score = Math.floor((Math.random() * 100) + 50);
         this.image = image;
-        this.dance = dance;
+      
     }
 
-    makeCard() {
-     
+// Makes Ajax call
+getImage() {
+      
+    let input = this.name;
+    let api = "&api_key=dy0FmMLlA7vphv8GPrWfz8W18KNS9npL&limit=" + 1;
+    let queryURL =
+      "https://api.giphy.com/v1/gifs/search?q=" + "dancing " + input + api;
+
+     //The AJAX call
+     $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).done(function(response) {
+         
+          let stillimage = response.data[0].images.fixed_height_still.url;
+          console.log(stillimage)
+          let movingimage = response.data[0].images.fixed_height.url;
+          console.log(movingimage)
+
+          
+
+
+    
+        
+        let image = $("<img>");
+          image.attr("src", stillimage);
+          image.attr("data-still", stillimage);
+          image.attr("data-state", "still");
+          image.attr("data-animate", movingimage);
+          image.addClass("animate");
+          image.addClass("img-fluid");
+          console.log(input)
+         $("#" + input + "-img").append(image);
+ 
+ 
+
+})}
+
+
+
+ //Creates card   
+
+ makeCard() {
       
 template.clone().appendTo(pickerLine).attr("id", this.name);
-$("#"+ this.name + " h4:nth-child(2)").text(this.name);
-
-
-   
-           
+$("#"+ this.name + " div:nth-child(1)").attr("id",  this.name +"-img")
+$("#"+ this.name + " h4:nth-child(2)").text(this.name).attr("id", this.name + "-name");
+$("#"+ this.name + " p:nth-child(3)").attr("id", this.name + "-score");
+$("#"+ this.name + " a:nth-child(4)").attr("id", this.name + "-btn").attr("value", this.name)
+//$("+"+this.name + "-btn").click(clickButton(this.name))
+$("#" + this.name + "-score " + "span").attr("id", this.name + "-points").text(this.score)
+        
  }
+
 
  picked() {
-     let newCard= $("."+ this.name)
-    newCard.css('border', '10px solid black');
+     
+     $("#"+ this.name).css('border', '10px solid purple').addClass("picked");
+ }
+
+ notPicked() {
+    $("#"+ this.name).addClass("not-picked").fadeOut(3000);
  }
 }
-let r = new Dancer("carla", 4, "#", "#");
-r.makeCard()
+let carla = new Dancer("carla", 4, "./images/wembley.jpg", "#");
+//carla.makeCard()
 
-let k = new Dancer("ralph", 4, "#", "#");
-k.makeCard()
 
-let s= new Dancer("stan", 4, "#", "#");
-s.makeCard()
-s.picked()
+
+let ralph = new Dancer("smurf", 4, "#", "#");
+ralph.makeCard()
+ralph.notPicked()
+ralph.getImage()
+
+let stan= new Dancer("stan", 12, "#", "#");
+stan.makeCard()
+stan.picked()
+stan.getImage()
+
+$( ".btn" ).click(function() {
+    
+   
+    let selectedName = $(this).attr("value")
+    $("#"+ selectedName).css('border', '10px solid purple').addClass("picked");
+    console.log(selectedName)
+    
+
+    
+    
+  })
+
+
+
 
